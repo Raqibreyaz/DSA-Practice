65bf15e0c9beb40f8808e301aaeeef8c250fff97
Raquib Reyaz <127662092+Raqibreyaz@users.noreply.github.com>
2025-09-05 18:30:48 +0530

    0493-reverse-pairs .cpp 09-05-2025

---
+++ LeetCode/Hard/0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    int reversePairs(vector<int>& nums) {
+        return mergeSort(nums,0,nums.size()-1);
+    }
+
+private:
+    int merge(vector<int>& arr, int s, int m, int e) {
+        vector<int> temp;
+        temp.reserve(e - s + 1);
+
+        int i = s, j = m + 1;
+        while (i <= m && j <= e) {
+
+            if (arr[i] <= arr[j]) {
+                temp.push_back(arr[i]);
+                i++;
+            }
+            else {
+                temp.push_back(arr[j]);               
+                j++;
+            }
+        }
+        while (i <= m) {
+            temp.push_back(arr[i]);
+            i++;
+        }
+        while (j <= e) {
+            temp.push_back(arr[j]);
+            j++;
+        }
+
+        int revCount = countReversePairs(arr,s,m,e);
+
+        for (i = s; i <= e; i++) {
+            arr[i] = temp[i - s];
+        }
+
+        return revCount;
+    }
+
+    int mergeSort(vector<int>& arr, int s, int e) {
+        if (s >= e)
+            return 0;
+
+        int m = s + (e - s) / 2;
+
+        int leftRevCount = mergeSort(arr, s, m);
+        int rightRevCount = mergeSort(arr, m + 1, e);
+
+        // merging both the arrays after sorting
+        int revCountMerge = merge(arr, s, m, e);
+
+        return leftRevCount + rightRevCount + revCountMerge;
+    }
+
+    int countReversePairs(vector<int> & nums, int s,int m,int e){
+        int count = 0;
+        for(int  i=s,j=m+1; i <= m && j <= e;){
+            if(nums[i]/2 > nums[j]){
+                count += m - i +1;
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+
+        return count;
+    }
+};
