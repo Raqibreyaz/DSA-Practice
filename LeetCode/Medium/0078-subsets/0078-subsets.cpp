6e587f3d4e920f4aac53516e72fcea7c89b199d2
Raquib Reyaz <127662092+Raqibreyaz@users.noreply.github.com>
2025-09-09 10:15:20 +0530

    0078-subsets .cpp 09-09-2025

---
+++ LeetCode/Medium/0078-subsets/0078-subsets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        solve(nums,0,temp,ans);
+
+        return ans;
+    }
+
+private:
+    void solve(vector<int>& nums,int index,vector<int>& temp,vector<vector<int>>& ans){
+        if(index >= nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+
+        temp.push_back(nums[index]);
+        solve(nums,index+1,temp,ans);
+
+        temp.pop_back();
+        solve(nums,index+1,temp,ans);
+    }
+};
